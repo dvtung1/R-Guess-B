053d397b8d7dab97187b490760d041911abf29d7
Tung Dao <dvtung98@gmail.com>
2019-04-21 00:28:57 -0400

    add express framework and start the server

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+
 require("dotenv").config();
-const DB_ID = process.env.DB_ID;
+const app = express();
+
 mongoose
   .connect(
-    `mongodb+srv://dvtung1:${DB_ID}@cluster0-hrsq8.mongodb.net/test?retryWrites=true`
+    `mongodb+srv://dvtung1:${
+      process.env.DB_ID
+    }@cluster0-hrsq8.mongodb.net/test?retryWrites=true`,
+    { useNewUrlParser: true }
   )
   .then(() => {
     console.log("Connected to database");
@@ -11,3 +18,23 @@ mongoose
   .catch(() => {
     console.log("Connect database failed");
   });
+
+//use body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//to connect to front end
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+//if local, use 3000. otherwise, use the deploy service port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
